0f4750542e6cb208c5b7ec4a60c644257089ca50
kkurno <kunakorn.pgm@gmail.com>
2020-12-10 11:08:44 +0700

    fix gtag function to send arguments instead

--- src/core/index.ts
+++ src/core/index.ts
@@ -6,14 +6,14 @@ import { warn } from '../utils/console';
 import { checkIsBrowser } from '../utils/compatibility';
 import { addGtagScriptDom } from '../utils/dom';
 
-export const gtag = (command: Command, ...arg: any[]) => {
+export function gtag(command: Command, ...arg: any[]) {
   if (!checkIsBrowser()) return;
   if (!GtagJsSingleton.getData().id) {
     warn(ErrorText.NEED_INITIALIZATION);
     return;
   }
   if (!window.dataLayer) window.dataLayer = [];
-  window.dataLayer.push([command, ...arg]);
+  window.dataLayer.push(arguments);
 };
 
 export const initialize = (id: string, option?: { gtag?: any, lib?: Omit<GtagJsSingletonData, 'id'> }) => {
